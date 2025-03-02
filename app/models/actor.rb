09c22e5a6460c26a6e5fb7bff68c18b4f413013d
jessie5557 <ychen38@chicagobooth.edu>
2025-03-02 05:27:49 +0000

    completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -15,6 +15,6 @@ class Actor < ApplicationRecord
 
   has_many(:characters, class_name: "Character", foreign_key: "actor_id")
 
-  has_many(:filmography, through: :characters, source: : movie)
+  has_many(:filmography, through: :characters, source: :movie)
 
 end
